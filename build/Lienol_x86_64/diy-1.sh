47f7023232b7fa4ebea4d44fdddc750d099be0fc
ixxmu <ixxmu@users.noreply.github.com>
2021-10-25 11:22:03 +0800

    Update diy-1.sh

--- build/Lienol_x86_64/diy-1.sh
+++ build/Lienol_x86_64/diy-1.sh
@@ -8,7 +8,7 @@
 # 软件包地址：https://github.com/281677160/openwrt-package
 # 拉取插件请看《各种命令的简单介绍》第4条、第5条说明,不管大神还是新手请认真的看看,再次强调请不要一下子就拉取别人一堆插件的插件包,容易造成编译错误的
 # 增加出国源
- sed -i '$a src-git luci-app-ssr-plus https://github.com/fw876/helloworld' feeds.conf.default
+ sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 # 你可能需要删除lienol的插件
 # Uncomment a feed source，如果用lede的源码只用加上这一句就可以看见ssr插件了！不用再次添加源的
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
@@ -18,7 +18,9 @@
 
 # svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-sfe package/diy/luci-app-sfe
 # svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-flowoffload package/diy/luci-app-flowoffload
+
 svn co https://github.com/281677160/openwrt-package/branches/19.07/feeds/luci/applications/luci-app-gowebdav package/luci-app-gowebdav
+
 #真的是每一个都不一样，lienol维护的是19.07的主分支，如果需要添加passwall添加下面2行就可以，第二个是依赖，跟新的也算频繁，貌似很稳定，该有的插件都有了
  sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
  sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
