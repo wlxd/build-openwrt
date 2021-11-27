9f8e07ae62c63572d0eba293c6cb0de69c3dbcbc
ixxmu <ixxmu@users.noreply.github.com>
2021-11-27 11:52:28 +0800

    Update diy-2.sh

--- build/Project_x86_64/diy-2.sh
+++ build/Project_x86_64/diy-2.sh
@@ -9,26 +9,26 @@
 
 
 
-cat > package/base-files/files/etc/networkip <<-EOF
-uci set network.lan.ipaddr='192.168.3.2'                                    # IPv4 地址(openwrt后台地址)
-uci set network.lan.netmask='255.255.255.0'                                 # IPv4 子网掩码
-uci set network.lan.gateway='192.168.3.1'                                   # IPv4 网关
-uci set network.lan.broadcast='192.168.3.255'                               # IPv4 广播
-uci set network.lan.dns=''                         # DNS(多个DNS要用空格分开)
-uci set network.lan.delegate='0'                                            # 去掉LAN口使用内置的 IPv6 管理
-uci commit network                                                          # 不要删除跟注释,除非上面全部删除或注释掉了
+#cat > package/base-files/files/etc/networkip <<-EOF
+#uci set network.lan.ipaddr='192.168.3.2'                                    # IPv4 地址(openwrt后台地址)
+#uci set network.lan.netmask='255.255.255.0'                                 # IPv4 子网掩码
+#uci set network.lan.gateway='192.168.3.1'                                   # IPv4 网关
+#uci set network.lan.broadcast='192.168.3.255'                               # IPv4 广播
+#uci set network.lan.dns=''                         # DNS(多个DNS要用空格分开)
+#uci set network.lan.delegate='0'                                            # 去掉LAN口使用内置的 IPv6 管理
+#uci commit network                                                          # 不要删除跟注释,除非上面全部删除或注释掉了
 #uci set dhcp.lan.ignore='1'                                                 # 关闭DHCP功能
 #uci commit dhcp                                                             # 跟‘关闭DHCP功能’联动,同时启用或者删除跟注释
-uci set system.@system[0].hostname='MyRouter'                            # 修改主机名称为OpenWrt-123
+#uci set system.@system[0].hostname='MyRouter'                            # 修改主机名称为OpenWrt-123
 #sed -i 's/\/bin\/login/\/bin\/login -f root/' /etc/config/ttyd             # 设置ttyd免帐号登录，如若开启，进入OPENWRT后可能要重启一次才生效
-EOF
+#EOF
 
 
 # sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile            # 选择argon为默认主题
 
-sed -i "s/OpenWrt /${Author} Compiled in $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/emortal/default-settings/files/zzz-default-settings  # 增加个性名字${Author}默认为你的github账号
+# sed -i "s/OpenWrt /${Author} Compiled in $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/emortal/default-settings/files/zzz-default-settings  # 增加个性名字${Author}默认为你的github账号
 
-sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/emortal/default-settings/files/zzz-default-settings        # 设置密码为空
+sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/emortal/default-settings/files/zzz-default-settings   # 设置密码为空
 
 #sed -i 's/PATCHVER:=4.19/PATCHVER:=4.14/g' target/linux/x86/Makefile                               # x86机型,默认内核4.19，修改内核为4.14或者4.9
 
@@ -38,8 +38,8 @@ sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/emortal/default-settings/files/zzz-def
 
 
 # 在线更新时，删除不想保留固件的某个文件，在EOF跟EOF直接加入删除代码，记住这里对应的是固件的文件路径，比如： rm /etc/config/luci
-cat >$DELETE <<-EOF
-EOF
+#cat >$DELETE <<-EOF
+#EOF
 
 
 # 修改插件名字
@@ -56,12 +56,12 @@ sed -i 's/"Argon 主题设置"/"Argon设置"/g' `grep "Argon 主题设置" -rl .
 
 
 # 整理固件包时候,删除您不想要的固件或者文件,让它不需要上传到Actions空间
-cat >${GITHUB_WORKSPACE}/Clear <<-EOF
-rm -rf config.buildinfo
-rm -rf feeds.buildinfo
-rm -rf openwrt-x86-64-generic-kernel.bin
-rm -rf openwrt-x86-64-generic.manifest
-rm -rf openwrt-x86-64-generic-squashfs-rootfs.img.gz
-rm -rf sha256sums
-rm -rf version.buildinfo
-EOF
+#cat >${GITHUB_WORKSPACE}/Clear <<-EOF
+#rm -rf config.buildinfo
+#rm -rf feeds.buildinfo
+#rm -rf openwrt-x86-64-generic-kernel.bin
+#rm -rf openwrt-x86-64-generic.manifest
+#rm -rf openwrt-x86-64-generic-squashfs-rootfs.img.gz
+#rm -rf sha256sums
+#rm -rf version.buildinfo
+#EOF
