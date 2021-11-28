0d73318ce7876837d52e587255ecf9d694432a32
ixxmu <ixxmu@users.noreply.github.com>
2021-11-28 18:39:25 +0800

    Update diy-1.sh

--- build/Lienol_x86_64/diy-1.sh
+++ build/Lienol_x86_64/diy-1.sh
@@ -60,5 +60,5 @@ rm -rf feeds/packages/libs/libcap
 svn co https://github.com/openwrt/packages/branches/openwrt-21.02/libs/libcap/ feeds/packages/libs/libcap
 
  # 添加ddnsto
- sed -i '$a src-git nas https://github.com/linkease/nas-packages.git;master' feeds.conf.default
- sed -i '$a src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' feeds.conf.default
+sed -i '$a src-git nas https://github.com/linkease/nas-packages.git;master' feeds.conf.default
+sed -i '$a src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' feeds.conf.default
