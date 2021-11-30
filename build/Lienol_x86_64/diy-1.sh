6aeb640cffdcfb867f1c13a9fe6326788f88aae1
ixxmu <ixxmu@users.noreply.github.com>
2021-11-30 20:04:03 +0800

    Update diy-1.sh

--- build/Lienol_x86_64/diy-1.sh
+++ build/Lienol_x86_64/diy-1.sh
@@ -67,4 +67,8 @@ git clone https://github.com/xiaorouji/openwrt-passwall package/passwall
 sed -i '$a src-git nas https://github.com/linkease/nas-packages.git;master' feeds.conf.default
 sed -i '$a src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' feeds.conf.default
 
+./scripts/feeds update nas nas_luci
+./scripts/feeds install -a -p nas
+./scripts/feeds install -a -p nas_luci
+
 
