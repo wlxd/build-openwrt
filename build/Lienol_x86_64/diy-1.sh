7393775dc7389ddfaf4167224b5d42bf39e3035e
ixxmu <ixxmu@users.noreply.github.com>
2021-11-30 19:23:30 +0800

    Update diy-1.sh

--- build/Lienol_x86_64/diy-1.sh
+++ build/Lienol_x86_64/diy-1.sh
@@ -56,8 +56,8 @@
 #　svn co https://github.com/immortalwrt/packages/trunk/net/pdnsd-alt package/helloworld/pdnsd-alt
 #　svn co https://github.com/immortalwrt/packages/trunk/net/redsocks2 package/helloworld/redsocks2
 
-rm -rf feeds/packages/libs/libcap
-svn co https://github.com/openwrt/packages/branches/openwrt-21.02/libs/libcap/ feeds/packages/libs/libcap
+# rm -rf feeds/packages/libs/libcap
+# svn co https://github.com/openwrt/packages/branches/openwrt-21.02/libs/libcap/ feeds/packages/libs/libcap
 
  # 添加ddnsto
 sed -i '$a src-git nas https://github.com/linkease/nas-packages.git;master' feeds.conf.default
