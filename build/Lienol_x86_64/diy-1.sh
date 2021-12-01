f29e3955363203e3d043ab19cc7e256e7fd73456
ixxmu <ixxmu@users.noreply.github.com>
2021-12-01 14:15:57 +0800

    Update diy-1.sh

--- build/Lienol_x86_64/diy-1.sh
+++ build/Lienol_x86_64/diy-1.sh
@@ -66,4 +66,4 @@ svn co https://github.com/openwrt/packages/branches/openwrt-21.02/libs/libcap/ f
  # 添加ddnsto, lieonl下无法添加成功,问题已经解决，可以正常添加了。
 # sed -i '$a src-git nas https://github.com/linkease/nas-packages.git;master' feeds.conf.default
 # sed -i '$a src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' feeds.conf.default
-# git clone https://github.com/kenzok8/small-package package/small-package
+git clone https://github.com/kenzok8/small-package package/small-package
