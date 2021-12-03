5f8a3ad163996fd78fa09c2f02b22f566deecb91
ixxmu <ixxmu@users.noreply.github.com>
2021-12-03 13:39:31 +0800

    Update diy-1.sh

--- build/Lienol_x86_64/diy-1.sh
+++ build/Lienol_x86_64/diy-1.sh
@@ -60,7 +60,7 @@ rm -rf feeds/packages/libs/libcap
 svn co https://github.com/openwrt/packages/branches/openwrt-21.02/libs/libcap/ feeds/packages/libs/libcap
 # git clone https://github.com/kenzok8/litte package/litte
 git clone https://github.com/kenzok8/jell package/jell
-git clone https://github.com/xiaorouji/openwrt-passwall package/passwall
+# git clone https://github.com/xiaorouji/openwrt-passwall package/passwall
 
 # git clone https://github.com/kenzok78/jeii package/jeii
 # git clone https://github.com/kenzok8/litte package/litte
