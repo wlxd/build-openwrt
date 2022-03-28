dc0b1908a6c353d1188905bf29127648dfbc0348
ixxmu <ixxmu@users.noreply.github.com>
2022-03-28 13:47:52 +0800

    Update diy-1.sh

--- build/Lienol_x86_64/diy-1.sh
+++ build/Lienol_x86_64/diy-1.sh
@@ -75,5 +75,5 @@ git clone https://github.com/immortalwrt-collections/openwrt-gowebdav package/di
 # sed -i '$a src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' feeds.conf.default
 
   git clone https://github.com/kenzok8/small-package package/small-package
-  git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/diy/luci-app-unblockneteasemusic
+# git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/diy/luci-app-unblockneteasemusic
 
