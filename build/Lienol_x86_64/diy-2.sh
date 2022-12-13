4484ef269c591ea79bc589bb3f426afa3c135336
ixxmu <ixxmu@users.noreply.github.com>
2022-12-13 15:21:24 +0800

    Update diy-2.sh

--- build/Lienol_x86_64/diy-2.sh
+++ build/Lienol_x86_64/diy-2.sh
@@ -34,9 +34,7 @@ sed -i 's/192.168.1.1/192.168.3.2/g' package/base-files/files/bin/config_generat
 # find . -name 'luci-app-wrtbwmon' -o -name 'wrtbwmon' -o -name 'pdnsd-alt' | xargs -i rm -rf {}
 # find . -name 'UnblockNeteaseMusic-Go' -o -name 'UnblockNeteaseMusic' -o -name 'luci-app-unblockmusic' | xargs -i rm -rf {}
 
-pushd package/small-packageluci-app-ssr-plus
-git reset --hard 7842f18c33019ec281083ff3f88294fbe5a89bda
-popd
+git reset --hard 7842f18c33019ec281083ff3f88294fbe5a89bda package/small-packageluci-app-ssr-plus
 
 # 更换golang版本，因为19.07自带golang无法编译xray的新版本
 pushd feeds/packages/lang
