b2ba37dbc7c95f8d3a1dd3e5f00ab3769de02338
ixxmu <ixxmu@users.noreply.github.com>
2025-07-10 14:26:09 +0800

    Add files via upload

---
+++ build/Lienol_x86_64_24/diy-1.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# 基本不需要添加啥插件了,我搜集了各位大神的插件都集成一个软件包直接打入源码里面了
+# 要了解增加了什么东西，就到我的专用软件包里面看看看吧，如果还是没有你需要的插件，请不要一下子就拉取别人的插件包
+# 相同的文件都拉一起，因为有一些可能还是其他大神修改过的容易造成编译错误的
+# 想要什么插件就单独的拉取什么插件就好，或者告诉我，我把插件放我的插件包就行了
+# 软件包地址：https://github.com/281677160/openwrt-package
+# 拉取插件请看《各种命令的简单介绍》第4条、第5条说明,不管大神还是新手请认真的看看,再次强调请不要一下子就拉取别人一堆插件的插件包,容易造成编译错误的
+# 增加出国源
+# sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+# 切换增加源的方式
+# sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+# 你可能需要删除lienol的插件
+# Uncomment a feed source，如果用lede的源码只用加上这一句就可以看见ssr插件了！不用再次添加源的
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# 发现天灵的是最新的
+# 这里注意你如果要在lieonl的固件中使用acc加速，你必须选择 DNSForwarder把它也加上，因为加速里面 目前仅支持 PDNSD 和 DNSForwarder，而此固件有没有内置；
+# 天灵acc 不能加，一加就编译失败 svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-19.07/package/ctcgfw/luci-app-turboacc package/diy/luci-app-turboacc
+
+# svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-sfe package/diy/luci-app-sfe
+# svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-flowoffload package/diy/luci-app-flowoffload
+
+#　svn co https://github.com/281677160/openwrt-package/branches/19.07/feeds/luci/applications/luci-app-gowebdav package/luci-app-gowebdav
+
+#真的是每一个都不一样，lienol维护的是19.07的主分支，如果需要添加passwall添加下面2行就可以，第二个是依赖，跟新的也算频繁，貌似很稳定，该有的插件都有了
+
+# sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+# sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+
+ # git clone https://github.com/kenzok8/litte package/litte
+ #  git clone https://github.com/xiaorouji/openwrt-passwall package/passwall
+  
+# 下面这个据说是官方支持的passwall，最好和上面的不要同时启用，怕冲突！！！
+#  sed -i '$a src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall.git' feeds.conf.default
+
+# 可能再加一个官方ssrplus
+# git clone https://github.com/fw876/helloworld package/diy/helloworld
+
+# 这里展示的是另外一种拉取方式，可能更加稳妥？想要passwall的在这里开启，已经试过可以正常拉取的
+# git clone https://github.com/xiaorouji/openwrt-passwall package/diy/luci-app-passwall
+
+# 自定义添加？
+ # git clone -b 19.07 --single-branch https://github.com/281677160/openwrt-package
+ 
+# git clone https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
+# git clone https://github.com/fw876/helloworld package/luci-app-ssr-plus
+
+# mkdir -p package/helloworld
+# for i in "dns2socks" "microsocks" "ipt2socks" "pdnsd-alt" "redsocks2"; do \
+#   svn checkout "https://github.com/immortalwrt/packages/trunk/net/$i" "package/helloworld/$i"; \
+# done
+
+#　sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+#　svn co https://github.com/immortalwrt/packages/trunk/net/dns2socks package/helloworld/dns2socks
+#　svn co https://github.com/immortalwrt/packages/trunk/net/microsocks package/helloworld/microsocks
+#　svn co https://github.com/immortalwrt/packages/trunk/net/ipt2socks package/helloworld/ipt2socks
+#　svn co https://github.com/immortalwrt/packages/trunk/net/pdnsd-alt package/helloworld/pdnsd-alt
+#　svn co https://github.com/immortalwrt/packages/trunk/net/redsocks2 package/helloworld/redsocks2
+
+#最好不要替换，替换后固件会变慢！
+# rm -rf feeds/packages/libs/libcap
+# svn co https://github.com/openwrt/packages/branches/openwrt-21.02/libs/libcap/ feeds/packages/libs/libcap
+
+# little 自带了ddnsto
+# git clone https://github.com/kenzok8/litte package/litte  
+git clone https://github.com/immortalwrt-collections/openwrt-gowebdav package/diy2
+# git clone https://github.com/kenzok8/jell package/jell
+# git clone https://github.com/xiaorouji/openwrt-passwall package/passwall
+
+# git clone https://github.com/kenzok78/jeii package/jeii
+# git clone https://github.com/kenzok8/litte package/litte
+#　git clone https://github.com/xiaorouji/openwrt-passwall package/passwall
+
+ # 添加ddnsto, lieonl下无法添加成功,问题已经解决，可以正常添加了。
+# sed -i '$a src-git nas https://github.com/linkease/nas-packages.git;master' feeds.conf.default
+# sed -i '$a src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' feeds.conf.default
+
+# 取消默认的 autosamba 依赖的 luci-app-samba 到 slim 里
+
+# find  ./target/linuxfind  ./target/linux/ -maxdepth 2 -type f  -name Makefile -exec sed -i 's#luci-app-vlmcsd##' {} \;
+# / -maxdepth 2 -type f  -name Makefile -exec sed -i 's#luci-app-vlmcsd##' {} \;
+
+# git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/diy/luci-app-unblockneteasemusic
+# lieonl 的19.07 不用加这些主题，small-package自带啊，都有 貌似只有大雕的不行需要自己加
+# rm -rf package/lean/luci-theme-argon
+# Add luci-theme-argon 这是那个修改版
+# git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon # 适配的是18.06以及LEDE版本
+# git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon # 主线适配的是19.07
+# git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config.git
+# 这个大佬应该是把ssr和passwall的依赖分开了，导致编译失败，加上看看吧
+# git clone https://github.com/kenzok8/small-package package/small-package
+
+
+
+# 可能与yml文件内的编译命令有关系，用 sed 语句拉下来的包不生效，一直无法拉取最新的xray，所以这里同为改为git clone 命令，可以拉到最新的了
+# 这里的逻辑应该是在最开始就让它加载在feeds conf的默认里面，不折腾了~~~~ 新编译的会自动崩溃！
+git clone https://github.com/kenzok8/openwrt-packages package/kenzo
+git clone https://github.com/kenzok8/small package/small
+git clone https://github.com/yichya/luci-app-xray package/luci_app_xray
+
+## sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+## sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+
+# sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+# sed -i '$a src-git-full luci_app_xray https://github.com/yichya/luci-app-xray' feeds.conf.default
+
---
+++ build/Lienol_x86_64_24/diy-2.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+
+# 修改openwrt登陆地址,把下面的192.168.2.2修改成你想要的就可以了
+sed -i 's/192.168.1.1/192.168.3.2/g' package/base-files/files/bin/config_generate
+
+# 修改主机名字，把OpenWrt-123修改你喜欢的就行（不能使用中文）
+# sed -i '/uci commit system/i\uci set system.@system[0].hostname='CSCLEDE'' package/default-settings/files/zzz-default-settings
+
+# 内核显示增加自己个性名称（281677160 build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些为后期增加）
+# sed -i "s/OpenWrt /kkitown build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/default-settings/files/zzz-default-settings
+
+# 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# 修改内核版本
+#sed -i 's/KERNEL_PATCHVER:=4.14/KERNEL_PATCHVER:=4.19/g' target/linux/x86/Makefile
+# 删除lionl自带
+# rm -rf package/lean/luci-app-sfe
+# rm -rf package/lean/luci-app-flowoffload
+# 删除small自带的音乐插件，因为我这里不能用
+# rm -rf package/small-package/UnblockNeteaseMusic
+# rm -rf package/small-package/UnblockNeteaseMusic-Go
+
+# cp -Rf "${Home}"/build/common/LIENOL/diy/* "${Home}"
+
+# rm -rf package/feeds/lienol/luci-app-ssr-mudb-server
+# rm -rf feeds/packages/libs/libcap
+# rm -rf package/luci-app-passwall/{v2ray-core,v2ray-plugin,xray-core,xray-plugin}
+
+# find . -name 'luci-app-netdata' -o -name 'netdata' -o -name 'luci-theme-argon' | xargs -i rm -rf {}
+# find . -name 'ddns-scripts_aliyun' -o -name 'ddns-scripts_dnspod' -o -name 'luci-app-wol' | xargs -i rm -rf {}
+# find . -name 'luci-app-wrtbwmon' -o -name 'wrtbwmon' -o -name 'pdnsd-alt' | xargs -i rm -rf {}
+# find . -name 'UnblockNeteaseMusic-Go' -o -name 'UnblockNeteaseMusic' -o -name 'luci-app-unblockmusic' | xargs -i rm -rf {}
+
+# 这个是xray 1.6.4 锁死版, 这个替换是没有问题的
+pushd package/small/xray-core
+git reset --hard 8abfcaf24ee8e19c5b838dd355515aad91c6af85
+popd
+
+# 更换golang版本，因为19.07自带golang无法编译xray的新版本；发现同一个配置编译出来体积一样但是这个慢了30分钟，所以保留这种此种替换写法
+# pushd feeds/packages/lang
+# rm -fr golang && svn co https://github.com/openwrt/packages/trunk/lang/golang
+# popd
+
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+
+
+# 尝试压缩xray 没有用
+# sed -i 's/PKG_BUILD_DEPENDS:=\(.*\)/PKG_BUILD_DEPENDS:=\1 upx\/host/' package/small/xray-core/Makefile
+# sed -i -e "/$(INSTALL_BIN) \$(PKG_INSTALL_DIR)\/usr\/bin\/main \$(1)\/usr\/bin\/xray/i\\t\$(STAGING_DIR_HOST)\/bin\/upx --lzma --best \$(PKG_INSTALL_DIR)\/usr\/bin\/main" package/small/xray-core/Makefile
+# 最原始命令 sed -i -e "/\/usr\/bin\/xray/a  \\\t$\(STAGING_DIR_HOST\)\/bin\/upx --lzma --best $\(1\)\/usr\/bin\/xray" package/small/xray-core/Makefile
+# sed -i -e "/\/usr\/bin\/xray/a  \\\t$\(STAGING_DIR_HOST\)\/bin\/upx --lzma --best --overlay=strip $\(1\)\/usr\/bin\/xray" package/small/xray-core/Makefile
+
+# 新增可能冲突插件
+# rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd-alt,miniupnpd-iptables,wireless-regdb}
+
+# 来自 kenzok8/openwrt_Build
+rm -rf feeds/luci/themes/luci-theme-argon && rm -rf feeds/other/luci-app-adguardhome && rm -rf feeds/packages/net/adguardhome
+rm -rf feeds/lienol/luci-app-fileassistant && rm -rf feeds/other/luci-app-dockerman && rm -rf feeds/packages/net/smartdns
+# cp -f feeds/smpackage/.github/diy/zzz-default-settings package/default-settings/files/zzz-default-settings
+# cp -f feeds/smpackage/.github/diy/banner package/base-files/files/etc/banner && rm -rf feeds/smpackage/upx
+rm -rf feeds/smpackage/luci-theme-design && git clone -b js --single-branch https://github.com/gngpp/luci-theme-design feeds/smpackage/luci-theme-design
+# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# sed -i "s/%D %V, %C/openwrt $(date +'%m.%d') by kenzo/g" package/base-files/files/etc/banner
+
+list="\
+  autosamba                ddns-scripts_aliyun             ddns-scripts-cloudflare ddns-scripts_dnspod \
+  luci-app-accesscontrol   luci-app-adbyby-plus            luci-app-arpbind        luci-app-autoreboot \
+  luci-app-control-timewol luci-app-control-webrestriction luci-app-control-weburl luci-app-cpufreq \
+  luci-app-ddns            luci-app-filetransfer           luci-app-ipsec-vpnd     luci-app-nlbwmon \
+  luci-app-ramfree         luci-app-ssr-plus               luci-app-timecontrol    luci-app-turboacc \
+  luci-app-unblockmusic    luci-app-unblockmusic           luci-app-upnp           luci-app-vlmcsd \
+  luci-app-vsftpd          luci-app-wol                    luci-app-zerotier \
+"
+for i in $list; do
+  sed -i "/DEFAULT_PACKAGES:=/,/^\s*$/s/$i//" include/target.mk
+  sed -i "/DEFAULT_PACKAGES/,//s/$i//" target/linux/ramips/Makefile
+done
+
+# 修改插件名字（修改名字后不知道会不会对插件功能有影响，自己多测试）
+sed -i 's/"aMule设置"/"电驴下载"/g' `grep "aMule设置" -rl ./`
+sed -i 's/"网络存储"/"NAS"/g' `grep "网络存储" -rl ./`
+sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' `grep "Turbo ACC 网络加速" -rl ./`
+sed -i 's/"实时流量监测"/"流量"/g' `grep "实时流量监测" -rl ./`
+sed -i 's/"KMS 服务器"/"KMS激活"/g' `grep "KMS 服务器" -rl ./`
+sed -i 's/"TTYD 终端"/"命令窗"/g' `grep "TTYD 终端" -rl ./`
+sed -i 's/"USB 打印服务器"/"打印服务"/g' `grep "USB 打印服务器" -rl ./`
+sed -i 's/"Web 管理"/"Web"/g' `grep "Web 管理" -rl ./`
+sed -i 's/"管理权"/"改密码"/g' `grep "管理权" -rl ./`
+sed -i 's/"带宽监控"/"监控"/g' `grep "带宽监控" -rl ./`
+sed -i 's/"Argon 主题设置"/"Argon设置"/g' `grep "Argon 主题设置" -rl ./`
