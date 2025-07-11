c3dfd0d31970618e845112a4a4ceb91037a6eaab
ixxmu <ixxmu@users.noreply.github.com>
2025-07-11 14:20:32 +0800

    Update diy-2.sh

--- build/Lienol_x86_64_23/diy-2.sh
+++ build/Lienol_x86_64_23/diy-2.sh
@@ -89,7 +89,9 @@ for i in $list; do
 done
 
 # 根据 https://github.com/kenzok8/openwrt_Build/blob/master/.github/workflows/Lienol.yml 改的
+
 sed -i 's/192.168.1.1/192.168.3.2/g' package/base-files/files/bin/config_generate
+
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # 修改插件名字（修改名字后不知道会不会对插件功能有影响，自己多测试）
