b9ddc425ab78be8d5992280b0c16070150a4ddd2
ixxmu <ixxmu@users.noreply.github.com>
2025-07-16 02:24:19 +0800

    Update diy-2.sh

--- build/Lienol_x86_64_23/diy-2.sh
+++ build/Lienol_x86_64_23/diy-2.sh
@@ -56,7 +56,7 @@ rm -rf feeds/luci/themes/luci-theme-argon && rm -rf feeds/other/{luci-app-adguar
 rm -rf feeds/lienol/luci-app-fileassistant && rm -rf feeds/packages/net/{adguardhome,smartdns}
 cp -f feeds/smpackage/.github/diy/zzz-default-settings package/default-settings/files/zzz-default-settings
 cp -f feeds/smpackage/.github/diy/banner package/base-files/files/etc/banner
-patch -p1 < "feeds/smpackage/.github/diy/patches/0004-luci-mod-status-firewall-disable-legacy-firewall-rul.patch"
+# patch -p1 < "feeds/smpackage/.github/diy/patches/0004-luci-mod-status-firewall-disable-legacy-firewall-rul.patch"
 rm -rf feeds/smpackage/luci-theme-design && git clone -b js --single-branch https://github.com/kenzok78/luci-theme-design feeds/smpackage/luci-theme-design
 
 # 来自 kenzok8/openwrt_Build
